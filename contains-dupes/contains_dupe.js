8b5491b37c27c89b054b407e6c48c563568e5f4c
Karim B <mattiakarim@live.it>
2020-05-25 14:46:55 +0200

    Complete js challenge for find duplicates

--- contains-dupes/contains_dupe.js
+++ contains-dupes/contains_dupe.js
@@ -1,3 +1,12 @@
 var containsDuplicate = function(nums) {
-
+    let dupObj = {};
+    for(idx in nums) {
+        if(nums[idx] in Object.keys(dupObj)) {
+            return console.log(true);
+        } else {
+            let num = nums[idx]
+            dupObj[num] = 1;
+        }
+    }
+    return console.log(false);
 }
